aa70a992bc0e3a605976de856ba8419f2b52935c
Jahanara Khatun <zarakhatun58@gmail.com>
2025-08-27 01:17:48 +0530

    update

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -25,7 +25,7 @@ export const createCheckoutSession = async (req, res) => {
       address,
       payment: {
         provider: 'stripe',
-        orderId: '', 
+        orderId: '',
       },
     });
 
@@ -52,7 +52,7 @@ export const createCheckoutSession = async (req, res) => {
 
     order.payment.orderId = session.id;
     await order.save();
-
+    console.log("CLIENT_URL:", process.env.CLIENT_URL)
     res.status(200).json({ sessionId: session.id, order });
   } catch (err) {
     console.error(err);
